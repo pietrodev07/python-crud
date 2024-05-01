0f113c074c821d048ecfdded4e0cd848067e75c9
pietrodev07 <pietro.dev.07@gmail.com>
2024-05-01 11:14:54 +0200

    fix: fixed typo

--- utils/data_handler.py
+++ utils/data_handler.py
@@ -4,7 +4,7 @@ def get_data(file_path: str) -> dict:
   with open(file_path, "r") as file:
     return json.load(file)
   
-def updata_data(file_path: str, users: list):
+def update_data(file_path: str, users: list):
   data = { "users": users }
   with open(file_path, "w") as file:
     return json.dump(data, file, indent=2)
